cb44c588142c082310f45734548d8e7682f96bb8
kc0115 <flanblue@gmail.com>
2026-04-21 11:34:46 -0600

    ultimos detalles

--- backend/database.py
+++ backend/database.py
@@ -2,15 +2,12 @@ import sqlite3
 import os
 
 def get_db_connection():
-    # Buscamos la ruta absoluta hacia tu carpeta database original
     ruta_db = os.path.join(os.getcwd(), "database", "arquikids.db")
     
     conn = sqlite3.connect(ruta_db)
     
-    # Permite que los resultados se manejen como diccionarios
     conn.row_factory = sqlite3.Row 
-    
-    # Activamos las llaves foráneas de SQLite por seguridad
+
     conn.execute("PRAGMA foreign_keys = ON;")
     
     return conn
